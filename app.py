eb4e5903feb531f34b4007f9586301334cbe3ea2
Sahaj Godhani <sahajgodhani777@gmail.com>
2021-08-04 17:42:29 +0530

    deploy done

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask,render_template,url_for,request
-import pickle
-import os
-import errno
-
-filename = 'pickle.pkl'
-clf = pickle.load(open(filename, 'rb'))
-cv=pickle.load(open('transform.pkl','rb'))
+from sklearn.feature_extraction.text import CountVectorizer
+import joblib
+from joblib import load
+pipeline = load("model.joblib")
+model = open('model.pkl','rb')
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	if request.method == 'POST':
-		message = request.form['content']
-		data = [message]
-		vect = cv.transform((data)).toarray()
-		my_prediction = clf.predict(vect)
-	return render_template('result.html',prediction = my_prediction)
+    #Alternative Usage of Saved Model
+    import spacy
+    #nlp = spacy.load('en_core_web_sm')
+    #bow_vector = CountVectorizer()
+    joblib.dump(pipeline, 'model.pkl')
+    model = open('model.pkl','rb')
+    model = joblib.load(model)
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        #vect = bow_vector.transform(data)
+        my_prediction = pipeline.predict(data)
+        return render_template('result.html',prediction = my_prediction)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
